e6a3888dd2eeee675d1bd625b54f1060e2ecc705
Satoru Katsumata <katsumata@vega.sd.tmu.ac.jp>
2018-04-22 21:36:43 +0900

    add somefile

---
+++ nmt_model.py
@@ -0,0 +1,162 @@
+import cupy
+import chainer
+import chainer.function as chainFunc
+import chainer.links as chainLinks
+import utility
+
+class BahdanauNMT(chainer.Chain):
+    def __init__(self):
+        super(BahdanauNMT, self).__init__()
+        with self.init_scope():
+            self.encoder = biGRU_encoder()
+            self.decoder = GRUDecoder()
+
+    def __call__(self, batch_src, batch_tgt):
+        self.reset_states()
+        hidden_encoder = self.encoder(batch_src)
+        last_h_enc = self.encoder.getHidden()
+        self.decoder.setHidden(last_h_enc)
+        loss, hyp = self.decoder(hidden_encoder, batch_tgt)
+        return loss, hyp
+
+    def generate(self, batch_src):
+        self.reset_states()
+        hidden_encoder = self.encoder(batch_src)
+        last_h_enc = self.encoder.getHidden()
+        self.decoder.setHidden(last_h_enc)
+        hyp = self.decoder(hidden_encoder)
+        return hyp
+
+    def reset_state(self):
+        self.encoder.reset_states()
+        self.decoder.reset_states()
+
+class biGRU_encoder(chainer.Chain):
+    def __init__(self, args):
+        super(biGRU_encoder, self).__init__()
+        with self.init_scope():
+            self.word2embed = chainLinks.EmbedID()
+            self.embed2hiddenf = chainLinks.GRU()
+            self.embed2hiddenb = chainLinks.GRU()
+        word2vec周りの処理
+        variable init
+        #self.dropoutr = args.dropoutrate
+
+    def __call__(self, batch):
+        first_states = list()
+        #forward_states = list()
+        backward_states = list()
+        concate_states = list()
+        #filstlayer   
+        for word in batch:
+            first_states.append(chainFunc.dropout(self.word2embed(word), self.dropoutr))
+        #backward
+        for first_hidden in first_states[::-1]:
+            backward_states.append(chainFunc.dropout(self.embed2hiddenb(first_hidden), self.dropoutr))
+        #cocate(forward,backward)
+        for first_hidden, hidden_b in zip(first_states, backward_states[::-1]):
+            forward_hidden = chainFunc.dropout(self.embed2hiddenf(first_hidden), self.dropoutr)
+            concate_states.append(chainFunc.concat((forward_hidden, hidden_b), axis=1))
+        return concate_states
+    
+    def reset_states(self):
+        self.embed2hiddenf.reset_state()
+        self.embed2hiddenb.reset_state()
+    
+    def getHidden(self):
+        #return backward hidden for decoder hidden initialization
+        return self.embed2hiddenb.h
+
+class GRUDecoder(chainer.Chain):
+    def __init__(self):
+        super(LSTMDecoder, self).__init__()
+        with self.init_scope():
+            word2embedding = chainLinks.EmbedID(ignore_label=-1)
+            self.gru = BahadanauGRU()
+            self.U_o = chainLinks.Linear()
+            self.V_o = chainLinks.Linear()
+            self.C_o = chainLinks.Linear()
+            self.W_o = chainLinks.Linear()
+            self.attention = addictiveAttention()
+
+    def __call__(self,enc_states, batch_tgt):
+        loss = chainer.Variable(self.xp.zeros((), dtype = self.xp.float32))
+        predicts = list()
+        for word in batch_tgt:
+            previous_hidden = self.gru.h
+            embedding = self.word2embedding(word)
+            context = self.attention(previous_hidden, enc_states)
+            hidden = self.gru(embedding, context)
+            t = self.U_o(previous_hidden) + self.V_o(embedding) + self.C_o(context)
+            t = self.chainFunc.maxout(t, 2)
+            score = self.W_o(t)
+            predict = chainFunc.argmax(score, axis=1)
+            loss += chainFunc.softmax_cross_entropy(score, word, ignore_label=-1)
+
+            predicts.append(predict.data)
+        return loss, predicts
+
+    def generate():
+        bos = self.xp.array([bosID]*batch_size, dtype=self.xp.int32)
+        predicts = [bos]
+        while len(predicts) < :
+            embedding = self.word2embedding(predicts[-1])
+            previous_hidden = self.gru.h
+            context = self.attention(previous_hidden, enc_states)
+            hidden = self.gru(embedding, context)
+            t = self.U_o(previous_hidden) + self.V_o(embedding) + self.C_o(context)
+            t = self.chainFunc.maxout(t, 2)
+            score = self.W_o(t)
+            predict = chainFunc.argmax(score, axis=1)
+
+            predicts.append(predict)
+        return predicts
+    
+    def setHidden(self, h):
+        self.gru.set_state(h)
+
+    def reset_states(self):
+        self.gru.reset_state()
+
+        
+class addictiveAttention(chainer.Chain):
+    def __init__(self):
+        super(addictiveAttention, self).__init__()
+        with self.init_scope():
+            self.W_a = chainLinks()
+            self.U_a = chainLinks()
+            self.V_a = chainLinks()
+    
+    def __call__(self, previous_hidden, enc_states):
+        weighted_hidden = self.W_a(previous_hidden)
+        scores = [self.V_a(chainFunc.tanh(weighted_hidden + self.U_a(hidden))) for hidden in enc_states]
+        align = chainFunc.softmax(scores)
+        context = chainFunc.sum(align*enc_states)
+        return context
+        
+
+class BahadanauGRU(chainLinks.StatefulGRU):
+    def __init__(self, in_size, out_size):
+        super(BahadanauGRU, self).__init__(in_size, out_size)
+        with self.init_scope():
+            self.C = chainLinks.Linear(2*out_size, out_size)
+            self.C_z = chainLinks.Linear(2*out_size, out_size)
+            self.C_r = chainLinks.Linear(2*out_size, out_size)
+
+    def __call__(self, word, context):
+        #like a statefulGRU code
+        z = self.W_z(word)
+        h_bar = self.W(word)
+        if self.h is not None:
+            r = chainFunc.sigmoid(self.W_r(word)+self.U_r(self.h)+self.C_r(context))
+            z += self.U_z(self.h) + self.C_z(context)
+            h_bar += self.U(r*self.h) + self.C(context)
+        z = chainFunc.sigmoid(z)
+        h_bar = chainFunc.tanh(h_bar)
+
+        if self.h is not None:
+            h_new = chainFunc.linear_interpolate(z, h_bar, self.h)
+        else:
+            h_new = z * h_bar
+        self.h = h_new
+        return self.h
---
+++ train.py
@@ -0,0 +1,88 @@
+import argparse
+import utilize as util
+import neural_model
+import chainer
+import cupy
+import random
+
+def train(args):
+    trace('start training...')
+
+    trace('random seed: {}'.format(args.seed_num))
+    np.random.seed(args.seed_num)
+    xp.random.seed(args.seed_num)
+    random.seed(args.seed_num)
+
+    chainer.global_config.train = True
+    chainer.global_config.use_cudnn = True
+    chainer.global_config.type_check = True
+    trace('chainer config: {}'.format(chainer.global_config.__dict__))
+
+    trace('load vocab...')
+    source_vocab = util.Vocab.load()
+    target_vocab = util.Vocab.load()
+    if args.gensim == 'make':
+        trace('making word2vec...')
+        source_word2vec = util.Embedding.make()
+        target_word2vec = util.Embedding.make()
+        source_word2vec.save()
+        target_word2vec.save()
+    elif args.gensim == 'load':
+        trace('loading word2vec...')
+        source_word2vec = util.Embedding.load()
+        target_word2vec = util.Embedding.load()
+    elif args.gensim == 'not':
+        trace('do not use word2vec')
+        source_word2vec = None
+        target_word2vec = None
+    
+    trace('making model...')
+    #initialize model
+    nmt_model = neural_model.hogehoge 
+    if args.gpunum >= 0:
+        import cupy as xp
+        cuda.check_cuda_available() 
+        cuda.get_device(args.gpunum).use()
+        trace('use GPU id: {}'.format(args.gpunum))
+    else:
+        import numpy as xp
+        args.gpunum = -1
+        trace('without GPU')
+
+    optim = args.optim
+    optim.setup(nmt_model)
+    optim.add_hook(chainer.optimizer.GradientClipping(args.grad_clip))
+
+        
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(usage='sorry, look at readme.', description='arg description', epilog='end')
+    parser.add_argument('sourcelang')
+    parser.add_argument('targetlang')
+    parser.add_argument('-datadir', help='data directory to use corpus and vocab', default='')
+    parser.add_argument('-savedir', help='save directory for weight', default='')
+    parser.add_argument('-model', help='model for neural MT', default='bahdanau')
+    parser.add_argument('-edim', help='embedding size for model'. type=int, default=512)
+    parser.add_argument('-nhid', help='hidden size for model'. type=int, default=512)
+    parser.add_argument('-gensim_mode', help='use gensim for embedding, make, load, or not?'. default='not', choices=['make', 'load', 'not'])
+    parser.add_argument('-gensimfileS', help='gensim file for source'. default='')
+    parser.add_argument('-gensimfileS', help='gensim file for target'. default='')
+    parser.add_argument('-nlayer', help='hidden layer for model, attention: 1layer using gensim is 2layer without gensim'. type=int, default=2)
+    parser.add_argument('-optim', help='select optimizer', default='AdaGrad')
+    parser.add_argument('-lr', help='learning rate for optimizer', type=float, default=0.01)
+    parser.add_argument('-gpunum', help='GPU number (negative value is using CPU)', type=int, default=-1)
+    parser.add_argument('-epoch', help='max epoch during training', type=int, default=50)
+    parser.add_argument('-useDropout', help='max epoch during training', action='store_true')
+    parser.add_argument('-dlr', help='dropout rate', type=float, default=0.2)
+    parser.add_argument('-batch', help='batch size', type=int, default=100)
+    parser.add_argument('-pooling', help='pooling size', type=int, default=100)
+    parser.add_argument('-genlimit', help='generation limit', type=int, default=60)
+    parser.add_argument('-useBeam', help='use beamsearch or not?', action='store_true')
+    parser.add_argument('-beamsize', help='beam size', type=int, default=2)
+    parser.add_argument('-grad_clip', help='gradient cliping', type=float, default=5.0)
+    parser.add_argument('-useSeed', help='use random seed or not?', action='store_true')
+    parser.add_argument('-seed_num', help='random seed number', type=int, default=2434)
+    args = parser.parse_args()
+    
+    train(args)
+    trace('finish training!')
